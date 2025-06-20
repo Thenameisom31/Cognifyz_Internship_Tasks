88ba33c764659fa11ea749cf96a2352a1cf74c4c
Thenameisom31 <omkarpatil3516@gmail.com>
2025-06-20 19:36:03 +0530

    Set up base directories for upcoming tasks (Level 1 & 2).

--- Level_2_Task/PasswordStrengthChecker.java
+++ Level_2_Task/PasswordStrengthChecker.java
@@ -7,20 +7,20 @@ public class PasswordStrengthChecker {
 
         if (password.length() >= 8) {
             check++;
-        } 
+        }
 
         if (password.matches(".*[A-Z].*")) {
 
             check++;
-        } 
+        }
 
         if (password.matches(".*[a-z].*")) {
             check++;
-        } 
+        }
 
         if (password.matches(".*\\d.*")) {
             check++;
-        } 
+        }
 
         if (password.matches(".*[!@#$%^&*(),.?\":{}|<>].*")) {
             check++;
@@ -33,17 +33,16 @@ public class PasswordStrengthChecker {
         System.out.print("Enter a password: ");
         String password = scanner.nextLine();
         int check = isStrongPassword(password);
-        
 
         if (check == 5) {
             System.out.println("The password is strong.");
-            System.out.println(check);
+
         } else if (check > 2 && check < 5) {
             System.out.println("The password is Moderate.");
-            System.out.println(check);
+
         } else {
             System.out.println("The password is weak.");
-            System.out.println(check);
+
         }
 
         scanner.close();
